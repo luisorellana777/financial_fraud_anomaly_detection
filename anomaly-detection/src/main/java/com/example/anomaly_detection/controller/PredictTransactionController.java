ea0989af7411d20fb11d4300722f0f11c63bfa25
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2025-01-06 17:55:02 -0500

    Collection

--- anomaly-detection/src/main/java/com/example/anomaly_detection/controller/PredictTransactionController.java
+++ anomaly-detection/src/main/java/com/example/anomaly_detection/controller/PredictTransactionController.java
@@ -3,7 +3,7 @@ package com.example.anomaly_detection.controller;
 import com.example.anomaly_detection.model.Transaction;
 import com.example.anomaly_detection.service.PredictiveService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ public class PredictTransactionController {
 
     private PredictiveService predictiveService;
 
-    @PutMapping(value = "/transaction/fraud")
+    @GetMapping(value = "/transaction/fraud")
     public boolean isFraud(@RequestBody Transaction transaction){
         return predictiveService.isFraud(transaction);
     }
